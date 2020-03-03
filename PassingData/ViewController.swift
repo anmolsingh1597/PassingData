3742a29e6a4b70500fd5f097164e4a20003afa02
Anmol Singh <anmolsingh1597@gmail.com>
2020-03-03 18:34:25 -0500

    Initial Commit

---
+++ PassingData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassingData
+//
+//  Created by Anmol singh on 2020-03-03.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
